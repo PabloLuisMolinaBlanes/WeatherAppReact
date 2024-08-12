1035ac40d26d8f6467e2fca8d378c36c364ea0e0
PabloLuisMolinaBlanes <pablomb255@gmail.com>
2024-08-12 15:46:24 +0200

    Added some basic elements to single-page app

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <p className="font-bold">Hello World!</p>
+    <div id="header" className="grid grid-flow-col">
+        <p className="text-3xl hover:text-blue-600 text-left font-bold">Settings</p>
+        <p className="text-3xl hover:text-blue-600 text-right font-bold">My list of places</p>
+    </div>
   )
 }
